2d3dd1b7a432264b47b66854f2c8c88433ae9282
Villanuevand <andresvillanuevab@gmail.com>
2015-03-27 23:07:34 -0430

    Funcionalidad Message

---
+++ message/message.controller.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Andres on 27/03/2015.
+ */
+(function(){
+    'use strict';
+    angular
+        .module('AppMessage')
+        .controller('MessageController',MessageController);
+    MessageController.$inject = ['$log','messageFactory'];
+    function MessageController($log,messageFactory){
+        $log.debug('cargo MessageController');
+        var vm = this;
+        vm.defecto = 10 // Número de mensajes por defecto
+        messages();
+
+        function messages(){
+            return messageFactory.getMessages()
+                .then(function(data){
+                    vm.messages = data;
+                    return vm.messages;
+                })
+                .catch(function(error){
+                    alert('Algo malo ocurrio! '+error);
+                });
+        }
+    }
+
+
+})();
---
+++ message/message.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Andres on 27/03/2015.
+ */
+(function(){
+    'use strict';
+    angular
+        .module('AppMessage',[{
+            name : 'message.factory',
+            files : ['services/message.factory.js']
+        }]);
+})();
